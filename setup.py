556806ce924adbe35921b01507bdfc95e8abcdf8
Steffen Hallmann <shallmann@km3net.de>
2025-12-11 01:41:07 +0100

    fix pip install

--- setup.py
+++ setup.py
@@ -1,4 +1,7 @@
 #!/usr/bin/env python
 """ Setuptools setup file for FCpy. """
-from setuptools import setup
-setup()
+from setuptools import setup, find_packages
+setup(
+    name="FCpy",
+    packages=find_packages()
+)
