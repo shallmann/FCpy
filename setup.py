3fe7ccc93d96ad6926604e36599a6d78f6cf2a46
Steffen Hallmann <shallmann@km3net.de>
2025-12-11 01:55:57 +0100

    test

--- setup.py
+++ setup.py
@@ -2,6 +2,13 @@
 """ Setuptools setup file for FCpy. """
 from setuptools import setup, find_packages
 setup(
-    name="FCpy",
-    packages=find_packages()
+       name="FCpy",
+       version="0.1.3",
+       packages=find_packages(),
+       python_requires=">=3.8",
+       install_requires=[
+           "numpy",
+           "scipy",
+           # optionally other deps
+       ],
 )
